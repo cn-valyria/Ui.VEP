9feabc87ca87e9eabf7a3d18771360bba89ff4b3
lilweirdward <zachw38@gmail.com>
2021-11-17 20:13:26 -0500

    More templating for the transactions page

--- src/client/src/app/admin/transactions/transactions.component.ts
+++ src/client/src/app/admin/transactions/transactions.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import bulmaCalendar from 'bulma-calendar';
 
 @Component({
   selector: 'app-transactions',
   templateUrl: './transactions.component.html',
   styleUrls: ['./transactions.component.scss']
 })
-export class TransactionsComponent implements OnInit {
+export class TransactionsComponent implements AfterViewInit {
+
+  @ViewChild("aidSinceCalendar") aidSinceCalendar: ElementRef<HTMLInputElement> | undefined;
+  @ViewChild("aidUntilCalendar") aidUntilCalendar: ElementRef<HTMLInputElement> | undefined;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    const aidSince = bulmaCalendar.attach(this.aidSinceCalendar?.nativeElement)[0];
+    aidSince.on("select", e => console.log(e));
+
+    const aidUntil = bulmaCalendar.attach(this.aidUntilCalendar?.nativeElement)[0];
+    aidUntil.on("select", e => console.log(e));
   }
 
 }
