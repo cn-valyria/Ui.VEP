d1c3a673b73090387c3d84277d92216970e6c595
lilweirdward <zachw38@gmail.com>
2022-05-20 00:14:14 -0400

    Initial creation of Nuxt client

---
+++ src/client-nuxt/components/NavBar.vue
@@ -0,0 +1,42 @@
+<template>
+  <nav class="navbar is-dark">
+    <div class="navbar-brand">
+      <a class="navbar-item">
+        <img src="~/assets/img/logo.png" />
+      </a>
+
+      <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+
+    <div class="navbar-menu">
+      <div class="navbar-start">
+        <!-- <a class="navbar-item">Home</a> -->
+        <NuxtLink to="/" class="navbar-item">Home</NuxtLink>
+        <a class="navbar-item">How It Works</a>
+      </div>
+
+      <div class="navbar-end">
+        <div class="navbar-item has-dropdown is-hoverable">
+          <a class="navbar-link is-arrowless">Admin Portal</a>
+          <div class="navbar-dropdown">
+            <!-- <a class="navbar-item" routerLink="admin">Home</a> -->
+            <NuxtLink to="/admin" class="navbar-item">Home</NuxtLink>
+            <a class="navbar-item" routerLink="admin/accounts">Accounts</a>
+            <a class="navbar-item" routerLink="admin/transactions">Transactions</a>
+            <a class="navbar-item">Lists</a> 
+          </div>
+        </div>
+
+        <a class="navbar-item">
+          <div class="buttons">
+            <a class="button is-primary">My Account</a>
+          </div>
+        </a>
+      </div>
+    </div>
+  </nav>
+</template>
---
+++ src/client-nuxt/pages/admin/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <NavBar />
+
+    <div class="container">
+      <div class="section">
+        <h1 class="title">VEP Admin Portal</h1>
+        <p>Admin page works!</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AdminIndex'
+}
+</script>
