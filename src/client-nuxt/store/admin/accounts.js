2baeb085fa39def9332520c231729f751bf9ffde
lilweirdward <zachw38@gmail.com>
2022-05-24 20:38:31 -0400

    Updated the store to actually hit the API

--- src/client-nuxt/store/admin/accounts.js
+++ src/client-nuxt/store/admin/accounts.js
@@ -43,47 +43,10 @@ export const mutations = {
 };
 
 export const actions = {
-  loadAllAccounts(context) {
-    const data = [
-      {
-        id: 1, 
-        nationId: 100236, 
-        nationName: "Azaroth", 
-        rulerName: "Wolfprince", 
-        discord: "", 
-        discordUniqueId: 0, 
-        uniqueCode: "79040", 
-        role: "B", 
-        alliance: "Freehold of The Wolves", 
-        activity: 1, 
-        strength: 449958, 
-        infra: 23463.1, 
-        tech: 65469.8, 
-        warStatus: 1, 
-        hasForeignMinistry: true, 
-        hasFederalAidCommission: true, 
-        hasDisasterReliefAgency: true, 
-        slotsUsed: 6, 
-        slotsFull: 6, 
-        totalCashSent: 1242, 
-        totalCashReceived: 0, 
-        totalTechSent: 0, 
-        totalTechReceived: 14500, 
-        credit: 9900, 
-        debt: 0, 
-        cashSentTechCredit: 10200, 
-        cashReceivedTechCredit: 0, 
-        cashSentCashCredit: 918, 
-        cashReceivedCashCredit: 0, 
-        techSentCashCredit: 0, 
-        techReceivedCashCredit: 27, 
-        techSentTechCredit: 0, 
-        techReceivedTechCredit: 300, 
-        previousListOrder: 999
-      }
-    ];
-    this.$log.info(data);
+  async loadAllAccounts(context) {
+    const response = await this.$axios.get("accounts");
+    this.$log.info(response);
     context.commit(CLEAR_ACCOUNTS);
-    context.commit(ADD_ACCOUNTS, data);
+    context.commit(ADD_ACCOUNTS, response.data);
   }
 };
