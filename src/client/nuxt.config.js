052083a7d945b06c44b8630d6d6287fd6ac07c6a
lilweirdward <zachw38@gmail.com>
2022-12-19 01:35:44 -0500

    More changes to the github actions config

--- src/client/nuxt.config.js
+++ src/client/nuxt.config.js
@@ -4,6 +4,9 @@ export default {
 
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
+  router: {
+    base: '/Ui.VEP/'
+  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
